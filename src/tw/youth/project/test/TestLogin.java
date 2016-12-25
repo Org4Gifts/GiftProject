5ca7ab3176b53a27b171d403bd32f70e975d75ac
user <user@NB-3TOJ3JD>
2016-12-26 00:29:10 +0800

    TestCase配合Login功能修改

--- src/tw/youth/project/test/TestLogin.java
+++ src/tw/youth/project/test/TestLogin.java
@@ -22,15 +22,15 @@ public class TestLogin {
 		System.out.println(login.checkLogin());
 		
 		System.out.println("測試修改密碼 : 舊密碼不正確範例");
-		System.out.println(login.changPassword(dao, "P0000", "P0002", "P0002"));
+		System.out.println(login.changPassword(dao, login.getUser(),"P0000", "P0002", "P0002"));
 		System.out.println("測試修改密碼 : 新密碼(1)不正確範例");
-		System.out.println(login.changPassword(dao, "P0001", "P0001", "P0002"));
+		System.out.println(login.changPassword(dao, login.getUser(),"P0001", "P0001", "P0002"));
 		System.out.println("測試修改密碼 : 新密碼(2)不正確範例");
-		System.out.println(login.changPassword(dao, "P0001", "P0002", "P0001"));
+		System.out.println(login.changPassword(dao, login.getUser(),"P0001", "P0002", "P0001"));
 		System.out.println("測試修改密碼 : 新舊密碼都正確範例");
-		System.out.println(login.changPassword(dao, "P0001", "P0002", "P0002"));
+		System.out.println(login.changPassword(dao, login.getUser(),"P0001", "P0002", "P0002"));
 		System.out.println("測試修改密碼 : 測試完畢，利用修改密碼方式來恢復成原密碼");
-		System.out.println(login.changPassword(dao, "P0002", "P0001", "P0001") + "\n");
+		System.out.println(login.changPassword(dao, login.getUser(),"P0002", "P0001", "P0001") + "\n");
 
 		System.out.println("測試發送電子郵件:不存在的電子郵件");
 		System.out.println(login.forgotPass(dao, "p0025@alaya.com","www.google.com")[0]);
